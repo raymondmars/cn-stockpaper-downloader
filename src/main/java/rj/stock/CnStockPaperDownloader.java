9692898153e7042209adeb8057ef0aea564db8e9
Robot Jiang <robot.z.jiang@gmail.com>
2016-12-06 09:03:07 +0800

    set jsoup download file maxBodySize to 2M

--- src/main/java/rj/stock/CnStockPaperDownloader.java
+++ src/main/java/rj/stock/CnStockPaperDownloader.java
@@ -23,6 +23,7 @@ public class CnStockPaperDownloader {
     private static final String CnStock_Paper_Host = "http://paper.cnstock.com";
     private static final String MockAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36";
     private static final int ReadTimeOut = 60*1000;
+    private static final int MaxDownloadFileSize = 2 * 1000 * 1000;  //set max download file to 2M
     private static final SimpleDateFormat Uf1 = new SimpleDateFormat("yyyy-MM/dd");
     private static final SimpleDateFormat Uf2 = new SimpleDateFormat("yyyyMMdd");
     private static final SimpleDateFormat StoreFolderFormat = new SimpleDateFormat("yyyy-MM-dd");
@@ -51,7 +52,7 @@ public class CnStockPaperDownloader {
         }
         for(int i = 1 ; i <= 10 ; i++) {
             String targetUrl = getDownloadUrl(i);
-            Connection.Response resp = Jsoup.connect(targetUrl).userAgent(MockAgent).cookies(map).timeout(ReadTimeOut).ignoreContentType(true).execute();
+            Connection.Response resp = Jsoup.connect(targetUrl).userAgent(MockAgent).cookies(map).maxBodySize(MaxDownloadFileSize).timeout(ReadTimeOut).ignoreContentType(true).execute();
             if(resp.statusCode() == 200 && resp.bodyAsBytes().length > 0) {
                 if(resp.contentType().equals("application/pdf")) {
                     FileOutputStream fs = new FileOutputStream(new File(String.format("%s/%d.pdf", storePath, i)));
